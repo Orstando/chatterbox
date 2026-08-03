a58a93eeccefce85a8643568f02ae7be610552cb
Orstando <173320602+Orstando@users.noreply.github.com>
2026-08-03 14:06:46 -0800

    admin panel sql

--- admin.js
+++ admin.js
@@ -3,10 +3,12 @@ const session = require('express-session');
 const bcrypt = require('bcryptjs');
 const ejs = require('ejs')
 
-const {readUsers, writeUsers} = require("./db")
+const { UserDatabase, MessageDatabase } = require("./db");
 const { TOKEN_SECRET, SESSION_SECRET, HTTP_PORT, SOCKET_PORT, WEBSOCKET_PORT, ROOMS } = require('./config');
 
 const router = express.Router();
+
+const userdb = new UserDatabase();
 
 router.get('/login', async (req, res) => {
   res.send(`
@@ -19,15 +21,11 @@ router.get('/login', async (req, res) => {
 });
 router.post('/login', async (req, res) => {
   const {username, password} = req.body;
-  const users = readUsers();
-  const user = users.users.find(user => user.admin && user.username === username);
-  if (!user || !(await bcrypt.compare(password, user.password))) {
+  const uid = await userdb.getUIDByName(username)
+  const user = await userdb.getUser(uid)
+  if (!(await userdb.isAdmin(uid)) || !(await bcrypt.compare(password, (new TextDecoder().decode(user.password))))) {
     console.log("Wrong password");
-    return res.status(403).send(`<p>Wrong password.</p><a href='/admin/login'>Go back</a>`);
-  }
-
-  if (!user) {
-    return res.status(403).send(`<p>Wrong password.</p><a href='/admin/login'>Go back</a>`);
+    return res.status(403).send(`<p>Incorrect username or password.</p><a href='/admin/login'>Go back</a>`);
   }
 
   req.session.admin = true;
@@ -60,17 +58,13 @@ router.get('/', async (req, res) => {
       </head>
       <body>
         <h2>User Negative Actions</h2>
-        <a style='color: red;' href='/admin/mute'>Mute User</a><br>
         <a style='color: red;' href='/admin/ban'>Ban User</a><br>
         <a style='color: red;' href='/admin/delete'>Delete User</a><br>
         <h2>User Positive Actions</h2>
-        <a style='color: green;' href='/admin/createAccount'>Create Account</a><br>
-        <a style='color: #33351c;' href='/admin/unmute'>Unmute User</a><br>
         <a style='color: #e79b0d;' href='/admin/unban'>Unban User</a><br>
         <h2>Miscellaneous</h2>
         <a style='color: blue;' href='/admin/userinfo'>Check User Information</a><br>
         <a style='color: blue;' href='/admin/userswithip'>Check Users with IP</a><br>
-        <a style='color: green;' href='/admin/changebanreason'>Change User Ban Reason</a><br>
       </body>
     </html>
   `);
@@ -110,13 +104,11 @@ router.post('/ban', async (req, res) => {
     return res.redirect("/admin/login");
   }
   const {username, reason} = req.body;
-  const users = readUsers();
-  const user = users.users.find(user => user.username === username);
+  const uid = await userdb.getUIDByName(username);
+  const user = await userdb.getUser(uid);
   if (user) {
-    user.banned = true;
-    user.banReason = reason || "No reason specified";
+    userdb.banUser(uid, reason)
   }
-  writeUsers(users);
 
   return res.send(`
     <p>User banned!</p>
@@ -157,18 +149,13 @@ router.post('/unban', async (req, res) => {
     return res.redirect("/admin/login");
   }
   const {username} = req.body;
-  const users = readUsers();
-  const user = users.users.find(user => user.username === username);
-  if (user) {
-    user.banned = false;
-    user.banReason = "";
-  }
-  writeUsers(users);
+  const uid = await userdb.getUIDByName(username);
+  await userdb.unbanUser(uid);
 
   return res.send(`
     <p>User unbanned!</p>
     <a href="/admin">Go back</a>
-    `);
+  `);
 });
 
 router.get('/delete', async (req, res) => {
@@ -203,176 +190,12 @@ router.post('/delete', async (req, res) => {
   if (!req.session.admin) {
     return res.redirect("/admin/login");
   }
-  const {username} = req.body;
-  const users = readUsers();
-  users.users = users.users.filter(user => user.username !== username);
-  writeUsers(users);
+  const { username } = req.body;
+  const uid = await userdb.getUIDByName(username);
+  await userdb.deleteUser(uid);
 
   return res.send(`
     <p>User deleted!</p>
-    <a href="/admin">Go back</a>
-    `);
-});
-router.get('/mute', async (req, res) => {
-  if (!req.session.admin) {
-    return res.redirect("/admin/login");
-  }
-  return res.send(`
-    <html>
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link rel="preload" as="style" href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" onload="this.onload=null;this.rel='stylesheet'">
-
-        <style>
-          h1, p, h2, a {
-            font-family: 'Roboto', Arial, sans-serif;
-          }
-        </style>
-      </head>
-      <body>
-      <h1 style='color: red;'>Mute User</h1>
-      <form method="POST">
-        <input name="username" placeholder="Username" /><br>
-        <button type="submit">Mute user</button>
-    </form>
-    </body>
-    </html>
-  `);
-});
-
-router.post('/mute', async (req, res) => {
-  if (!req.session.admin) {
-    return res.redirect("/admin/login");
-  }
-  const { username } = req.body;
-  const users = readUsers();
-  const user = users.users.find(user => user.username === username);
-  
-  // Commit the mute to the database
-  if (user) {
-    user.muted = true;
-    writeUsers(users);
-    return res.send(`
-      <p>User muted successfully.</p>
-      <a href="/admin">Go back</a>
-    `);
-  }
-
-  return res.send(`
-    <p>User not found.</p>
-    <a href="/admin">Go back</a>
-  `);
-});
-router.get('/unmute', async (req, res) => {
-  if (!req.session.admin) {
-    return res.redirect("/admin/login");
-  }
-  return res.send(`
-    <html>
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link rel="preload" as="style" href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" onload="this.onload=null;this.rel='stylesheet'">
-
-        <style>
-          h1, p, h2, a {
-            font-family: 'Roboto', Arial, sans-serif;
-          }
-        </style>
-      </head>
-      <body>
-      <h1 style='color: green;'>Unmute User</h1>
-      <form method="POST">
-        <input name="username" placeholder="Username" /><br>
-        <button type="submit">Unmute user</button>
-    </form>
-    </body>
-    </html>
-  `);
-});
-
-router.post('/unmute', async (req, res) => {
-  if (!req.session.admin) {
-    return res.redirect("/admin/login");
-  }
-  const { username } = req.body;
-  const users = readUsers();
-  const user = users.users.find(user => user.username === username);
-  
-  if (user) {
-    user.muted = false;
-    writeUsers(users);
-  }
-
-
-  return res.send(`
-    <p>User unmuted!</p>
-    <a href="/admin">Go back</a>
-  `);
-});
-
-router.get('/createAccount', async (req, res) => {
-  if (!req.session.admin) {
-    return res.redirect("/admin/login");
-  }
-  return res.send(`
-    <html>
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link rel="preload" as="style" href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" onload="this.onload=null;this.rel='stylesheet'">
-
-        <style>
-          h1, p, h2, a {
-            font-family: 'Roboto', Arial, sans-serif;
-          }
-        </style>
-      </head>
-      <body>
-      <h1 style='color: green;'>Create User</h1>
-      <form method="POST">
-        <input name="username" placeholder="Username" required /><br>
-        <input name="password" placeholder="Password" /><br>
-        <input name="passwordHash" placeholder="Password Hash" /><br>
-        <button type="submit">Create Account</button>
-    </form>
-    </body>
-    </html>
-  `);
-});
-
-router.post('/createAccount', async (req, res) => {
-  if (!req.session.admin) {
-    return res.redirect("/admin/login");
-  }
-  const {username, password, passwordHash} = req.body;
-  
-
-  const users = readUsers();
-  if (users.users.find(user => user.username === username)) {
-    console.log("Signup: account already in use");
-    return res.status(409).send("ERR_USER_USED");
-  }
-
-  var hashedPassword;
-  if (!passwordHash) {
-    hashedPassword = await bcrypt.hash(password, 10);
-  } else if (!password) {
-    hashedPassword = passwordHash
-  } else {
-      return res.send(`
-        <p>You need to at least include a password or a password hash.</p>
-        <a href="/admin">Go back</a>
-      `);
-  }
-  
-  const newUser = { id: Date.now().toString(), username, password: hashedPassword, admin: false, ip: req.ip, banned: false, banReason: "", muted: false };
-  users.users.push(newUser);
-  writeUsers(users);
-
-  return res.send(`
-    <p>User created!</p>
     <a href="/admin">Go back</a>
   `);
 });
@@ -409,13 +232,14 @@ router.post('/userinfo', async (req, res) => {
   if (!req.session.admin) {
     return res.redirect("/admin/login");
   }
-  const {username} = req.body;
+  const { username } = req.body;
   
-  const users = readUsers();
-  const user = users.users.find(user => user.username === username);
-
+  const uid = await userdb.getUIDByName(username);
+  var user = await userdb.getUser(uid);
+  user.banReason = (await userdb.isUserBanned(uid)).reason;
+  user.banned = !!user.banReason;
   return res.send(`
-    <p>Username: ${user.username}<br>Password Hash: ${user.password}<br>ID: ${user.id}<br>Banned: ${user.banned}<br>Ban Reason: ${user.banReason || "None"}<br>Muted: ${user.muted}<br>IP: <a href="#" onclick="return postName(this)">${user.ip}</a></p>
+    <p>Username: ${user.username}<br>Password Hash: ${(new TextDecoder().decode(user.password))}<br>ID: ${user.id}<br>Banned: ${user.banned}<br>Ban Reason: ${user.banReason || "None"}<br>IP: <a href="#" onclick="return postName(this)">${user.ip}</a></p>
     <a href="/admin">Go back</a>
     <script>
       function postName(el) {
@@ -435,7 +259,7 @@ router.post('/userinfo', async (req, res) => {
         return false;
       }
     </script>
-    `);
+  `);
 });
 
 router.get('/userswithip', async (req, res) => {
@@ -470,14 +294,16 @@ router.post('/userswithip', async (req, res) => {
   if (!req.session.admin) {
     return res.redirect("/admin/login");
   }
-  const {ip} = req.body;
-  
-  const users = readUsers()["users"];
-  const matches = users.filter(item => item.ip === ip).map(item => item.username);
-  const usernamesHtml = matches.map(username => `<a href="#" onclick="return postName(this)"><p>${username}</p></a>`).join("\n");
-
+  const { ip } = req.body;
+  const dbresult = await userdb.usersWithIp(ip);
+  var result = "";
+  dbresult.forEach((user) => {
+    result += "<li><a href='#'' onclick='return postName(this)'>"+user.username+"</a></li>";
+  })
   return res.send(`
-    ${usernamesHtml}
+    <ul>
+      ${result}
+    </ul>
     <a href="/admin">Go back</a>
     <script>
     function postName(el) {
@@ -499,54 +325,6 @@ router.post('/userswithip', async (req, res) => {
         return false;
     }
     </script>
-    `);
-});
-
-router.get('/changebanreason', async (req, res) => {
-  if (!req.session.admin) {
-    return res.redirect("/admin/login");
-  }
-  return res.send(`
-    <html>
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link rel="preload" as="style" href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" onload="this.onload=null;this.rel='stylesheet'">
-
-        <style>
-          h1, p, h2, a {
-            font-family: 'Roboto', Arial, sans-serif;
-          }
-        </style>
-      </head>
-      <body>
-      <h1 style='color: green;'>Change User Ban Reason</h1>
-      <form method="POST">
-        <input name="username" placeholder="Username" required /><br>
-        <input name="reason" placeholder="Reason" required /><br>
-        <button type="submit">Change Ban Reason</button>
-    </form>
-    </body>
-    </html>
-  `);
-});
-
-router.post('/changebanreason', async (req, res) => {
-  if (!req.session.admin) {
-    return res.redirect("/admin/login");
-  }
-  const {username, reason} = req.body;
-  const users = readUsers();
-  const user = users.users.find(user => user.username === username);
-  if (user) {
-    user.banReason = reason || "No reason specified";
-  }
-  writeUsers(users);
-
-  return res.send(`
-    <p>Changed user ban reason!</p>
-    <p>Reason: ${reason || "No reason specified"}</p>
-    <a href="/admin">Go back</a>
     `);
 });
 
--- db.js
+++ db.js
@@ -13,18 +13,23 @@ class UserDatabase {
         ip TEXT(15)
       );
       CREATE TABLE IF NOT EXISTS bannedusers (
-        id INTEGER,
+        id INTEGER PRIMARY KEY,
         ip TEXT(15),
         reason TEXT
       );
       CREATE TABLE IF NOT EXISTS admins (
-        id INTEGER,
-        type TINYINT
+        id INTEGER PRIMARY KEY,
+        type TEXT
       );
     `);
   }
   async getUIDByName(username) {
-    return this.db.prepare("SELECT id FROM users WHERE username = ?").get(username).id;
+    const prepared = this.db.prepare("SELECT id FROM users WHERE username = ?").get(username);
+    if (prepared) {
+      return prepared.id;
+    } else {
+      return null; // user doesnt exist
+    }
   }
   async getUser(uid) {
     return this.db.prepare("SELECT * FROM users WHERE id = ?").get(uid);
@@ -47,8 +52,17 @@ class UserDatabase {
   }
   async banUser(uid, reason) {
     reason = reason || "No reason specified"
-    const ip = this.db.prepare("SELECT ip FROM users WHERE id = ?").get(uid);
-    this.db.prepare("INSERT INTO bannedusers (id, ip, reason) VALUES (?, ?, ?)").run(uid, ip, reason);
+    const ip = this.db.prepare("SELECT ip FROM users WHERE id = ?").get(uid).ip;
+    this.db.prepare("INSERT OR REPLACE INTO bannedusers (id, ip, reason) VALUES (?, ?, ?)").run(uid, ip, reason);
+  }
+  async unbanUser(uid) {
+    this.db.prepare("DELETE FROM bannedusers WHERE id = ?").run(uid);
+  }
+  async deleteUser(uid) {
+    this.db.prepare("DELETE FROM users WHERE id = ?").run(uid);
+  }
+  async usersWithIp(ip) {
+    return this.db.prepare("SELECT * FROM users WHERE ip = ?").all(ip);
   }
   async doesUnameExist(username) {
     return !!this.db.prepare("SELECT * FROM users WHERE username = ?").get(username);
